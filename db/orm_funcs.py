df58f68621249ed20494edcb96c534b61a80fd55
lissk0 <lissk0@mail.ru>
2024-06-25 02:26:14 +0300

    overwritten create_db, insert_to_db. some bugs still need fixes

--- db/orm_funcs.py
+++ db/orm_funcs.py
@@ -1,12 +1,28 @@
-import sqlalchemy as sa
-import sqlalchemy.orm as orm
+import sqlalchemy
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, MetaData, Table, Column, String
 
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+from datetime import date
+
 POSTGRE_USERNAME = "postgres"
 POSTGRE_PASS = "2409"
+
+SqlAlchemyBase = sqlalchemy.orm.declarative_base()  # ??
+
+
+class Url(SqlAlchemyBase):
+    def __init__(self, token, long_url, creation_date):
+        self.token = token
+        self.long_url = long_url
+        self.creation_date = creation_date
+
+    __tablename__ = 'urls'
+    long_url = Column(String, primary_key=True)
+    token = Column(String)
+    creation_date = Column(String)
 
 
 def create_db():
@@ -14,42 +30,66 @@ def create_db():
     connection = psycopg2.connect(user=POSTGRE_USERNAME, password=POSTGRE_PASS)
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # при использовании уровня изоляции транзакции
                                                                 # ISOLATION_LEVEL_AUTOCOMMIT каждая операция INSERT,
-                                                                # UPDATE, DELETE или т.д. будет автоматически
+                                                                # UPDATE, DELETE и т.д. будет автоматически
                                                                 # фиксирована в базе данных без необходимости явного
                                                                 # вызова команды COMMIT.
 
+    # creating db
     cursor = connection.cursor()
-    cursor.execute('create database db_url')
+    try:
+        cursor.execute('create database db_url')
+    except psycopg2.errors.DuplicateDatabase:  # db has been already created
+        pass
     cursor.close()
     connection.close()
 
     # dialect+driver://username:password@host:port/db_name
     # default parameters: echo=False, pool_size=5, max_overflow=10, encoding='UTF-8'
     connection_link = "postgresql+psycopg2://" + POSTGRE_USERNAME + ":" + POSTGRE_PASS + "@localhost/db_url"
+
+    # engine creating and connecting
     engine = create_engine(connection_link)
-    engine.connect()
+    engine.connect()  # print(engine)
     metadata = MetaData()
 
-    # print(engine)
+    # creating the main table
+    table = Table('urls', metadata,
+                  Column('long_url', String, primary_key=True),
+                  Column('token', String),
+                  Column('creation_date', String))
 
-    table = Table(
-        'urls',
-        metadata,
-        Column('token', String, primary_key=True),
-        Column('long_url', String),
-        Column('creation_date', String)
-    )
-
-    # creating the table (if it doesn't exist)
+    # init the table (if it doesn't exist)
     metadata.create_all(bind=engine)
+    # SqlAlchemyBase.metadata.create_all(engine)  # хз в чем отличие, но это не работает
+
+    # cursor.execute("insert into db_url (token, long_url, creating_date) values ('TEST2', 'https', '24.02')")
+    # cursor.close()
+    # connection.close()
 
     return engine, table
 
 
-def insert_to_db(engine, table):
-    insert_query = table.insert().values(token='', long_url='', creation_date='')
-    with engine.connect() as connection:
-        connection.execute(insert_query)
+def insert_to_db(engine, table, long_url, token):
+    current_date = date.today().strftime("%d.%m.%Y")
+
+    # можно глобально прописать
+    # session = sessionmaker(bind=engine)
+    # и потом просто юзать session = Session()
+    # но мне понятнее каждый раз прописывать engine
+    with Session(engine) as session:
+
+        # url = Url()
+        # url.token = token
+        # url.long_url = long_url
+        # url.current_date = current_date
+        try:
+            session.add(Url(long_url, token, current_date))
+        except sqlalchemy.exc.IntegrityError:  # long_url is already in table
+            pass
+        except psycopg2.errors.UniqueViolation:  # long_url is already in table
+            pass
+
+        session.commit()  # print(session.new)
 
 
 def print_db(engine, table):
@@ -60,9 +100,20 @@ def print_db(engine, table):
             print(row)
 
 
-def is_in_db(url_long):
-    return True
+def is_in_db(engine, table, long_url):
+    select_query = table.select(where=long_url)
+    with engine.connect() as connection:
+        result = connection.execute(select_query)
+    if result:
+        return True
+    return False
 
 
 def get_short_url_from_db(url_long):
     return "https://short.url"
+
+
+engine, table = create_db()
+print_db(engine, table)
+insert_to_db(engine, table, "https://test.url/", "TEST1")
+print_db(engine, table)
