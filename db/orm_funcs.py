df92ec1efb2d0c0b574d91bee58d98d1ce015f31
lissk0 <105405909+LiSSk0@users.noreply.github.com>
2024-06-30 16:15:44 +0300

    DB to the master branch (#1)
    * added connection to DB, funcs insert & print db
    
    * overwritten create_db, insert_to_db. some bugs still need fixes
    
    * fixed bug with primary keys
    
    * changed insert_to_db, print_db. added get_table
    
    * +is_in_db, +get_token_from_db, changed insert_to_db
    
    * +credentials.txt for db log/pass, +get_data for credentials
    
    * changed date datatype from String to Date. changed db url record from creation_date to expiration_date
    
    * renamed 'get_data.py' to 'credentials_funcs.py'. fixed problems w/ bad credentials
    
    * +requirements.txt
    
    * +get_tokens. ready to be merged to master branch

--- db/orm_funcs.py
+++ db/orm_funcs.py
@@ -1,6 +1,131 @@
-def is_in_db(url_long):
-    return True
+import sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Date
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+from datetime import date, timedelta
+import sys
+
+from credentials_funcs import get_credentials, check_credentials
+
+DB_NAME = "db_url"
+POSTGRE_USERNAME, POSTGRE_PASS = get_credentials("credentials.txt")
+EXPIRATION_TIME = 30  # url retention time (days), after which it will be deleted
+
+SqlAlchemyBase = sqlalchemy.orm.declarative_base()  # ??
 
 
-def get_short_url_from_db(url_long):
-    return "https://short.url"
+class Url(SqlAlchemyBase):
+    def __init__(self, long_url, token, expiration_date):
+        self.token = token
+        self.long_url = long_url
+        self.expiration_date = expiration_date
+
+    __tablename__ = 'urls'
+    long_url = Column(String, primary_key=True)
+    token = Column(String)
+    expiration_date = Column(Date)
+
+
+def create_db(db_name):
+    # creating connection to postgres
+    connection = psycopg2.connect(user=POSTGRE_USERNAME, password=POSTGRE_PASS)
+    # except (psycopg2.OperationalError, sqlalchemy.exc.OperationalError):
+    #    print("!ERROR bad credentials: db/orm_funcs.py - create_db.")
+    #    return False
+
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # при использовании уровня изоляции транзакции
+                                                                # ISOLATION_LEVEL_AUTOCOMMIT каждая операция INSERT,
+                                                                # UPDATE, DELETE и т.д. будет автоматически
+                                                                # фиксирована в базе данных без необходимости явного
+                                                                # вызова команды COMMIT.
+
+    # creating db if not
+    cursor = connection.cursor()
+    try:
+        cursor.execute('create database ' + db_name)
+    except psycopg2.errors.DuplicateDatabase:  # db has been already created
+        pass
+    cursor.close()
+    connection.close()
+
+
+def connect_to_db(db_name):
+    # dialect+driver://username:password@host:port/db_name
+    # default parameters: echo=False, pool_size=5, max_overflow=10, encoding='UTF-8'
+    connection_link = "postgresql+psycopg2://" + POSTGRE_USERNAME + ":" + POSTGRE_PASS + "@localhost/" + db_name
+
+    # engine creating and connecting
+    engine = create_engine(connection_link)
+    engine.connect()  # print(engine)
+    metadata = MetaData()
+
+    # creating the main table
+    table = Table('urls', metadata,
+                  Column('long_url', String, primary_key=True),
+                  Column('token', String),
+                  Column('expiration_date', Date))
+
+    # init the table (if it doesn't exist)
+    metadata.create_all(bind=engine)
+    # SqlAlchemyBase.metadata.create_all(engine)  # хз в чем отличие, но не работает
+
+    return engine
+
+
+def is_in_db(engine, long_url):
+    with Session(engine) as session:
+        if session.query(Url).filter(Url.long_url == long_url).first():
+            return True
+        return False
+
+
+def insert_to_db(engine, long_url, token):
+    current_date = date.today()  # .strftime("%d.%m.%Y")
+    expiration_date = current_date + timedelta(days=EXPIRATION_TIME)  # "yyyy-mm-dd"
+
+    # можно глобально прописать
+    # session = sessionmaker(bind=engine)
+    # и потом просто юзать session = Session()
+    # но мне не хочется глобальные делать
+    result = is_in_db(engine, long_url)  # объявление тут, чтобы не было двойного открытия сессии ниже
+    with Session(engine) as session:
+        if not result:
+            session.add(Url(long_url, token, expiration_date))
+            session.commit()
+        else:
+            print("!ERROR inserting: db/orm_funcs.py - insert_to_db: url '" + long_url + "' is already in db.")
+
+
+def get_table(engine):
+    table = []
+    with Session(engine) as session:
+        all_urls = session.query(Url).all()
+        for url in all_urls:
+            table.append((url.long_url, url.token, url.expiration_date))
+    return table
+
+
+def print_table(engine):
+    table = get_table(engine)
+    for url in table:
+        print(*url)
+
+
+def get_token_from_db(engine, long_url):
+    if not is_in_db(engine, long_url):
+        return None
+    with Session(engine) as session:
+        return session.query(Url.token).filter(Url.long_url == long_url).first()[0]
+
+
+def get_tokens(engine):
+    with Session(engine) as session:
+        return session.query(Url.token).all()
+
+
+if not check_credentials(POSTGRE_USERNAME, POSTGRE_PASS):
+    print("!ERROR bad credentials")
+    # sys.exit()
