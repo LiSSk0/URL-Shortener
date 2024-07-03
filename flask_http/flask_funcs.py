26ea0ce5d902119a9607db0430f87d7639280f5b
kurlik-da <dasha_soloveva_2005@mail.ru>
2024-07-03 23:37:14 +0300

    pages are linked

--- flask_http/flask_funcs.py
+++ flask_http/flask_funcs.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def get_long_url():
-    return render_template("input_long_url.html")
 
-@app.route('/get_url', methods=['GET', 'POST'])
-def send_long_url():
-    return render_template("output_short_url.html")
+@app.route('/', methods=['GET', 'POST'])
+def input_long_url():
+    if request.method == 'GET':
+        return render_template("input_long_url.html")
+    else:
+        long_url = request.form.get('longurl')
+        print(long_url)
+        token = int(long_url)+5
+        return redirect(url_for('output_short_url', token=token))
+
+
+@app.route('/short_url', methods=['GET', 'POST'])
+def output_short_url():
+    token = request.args.get('token', None)
+    return render_template("output_short_url.html", token=token)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
