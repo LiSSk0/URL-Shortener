3a9cfe84f7640dbc469f0afee27b766e8e8f415b
kurlik-da <dasha_soloveva_2005@mail.ru>
2024-07-03 22:17:30 +0300

    pages are not empty

--- flask_http/flask_funcs.py
+++ flask_http/flask_funcs.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def get_long_url():
-    return "дай ссылку"
+    return render_template("input_long_url.html")
 
 @app.route('/get_url', methods=['GET', 'POST'])
 def send_long_url():
-    return "держи ссылку"
+    return render_template("output_short_url.html")
 
 if __name__ == '__main__':
     app.run()
