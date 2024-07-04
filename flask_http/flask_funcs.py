84efb518364872c3ca6f0720810ec9b9f7823c4a
kurlik-da <dasha_soloveva_2005@mail.ru>
2024-07-04 19:13:32 +0300

    all in one page

--- flask_http/flask_funcs.py
+++ flask_http/flask_funcs.py
@@ -33,18 +33,12 @@ def input_long_url():
         if long_url != '':
             print(long_url)
             if(is_in_db(long_url)):
-                token = get_token_from_db(long_url)
+                token = 'http://127.0.0.1:5000/' + get_token_from_db(long_url)
             else:
-                token = create_short_url(long_url)
-            return redirect(url_for('output_short_url', token=token))
+                token = 'http://127.0.0.1:5000/' + create_short_url(long_url)
+            return render_template("output_short_url.html", token=token)
         else:
             return render_template("error_long_url_is_None.html")
-
-
-@app.route('/short_url', methods=['GET', 'POST'])
-def output_short_url():
-    token = 'http://127.0.0.1:5000/'+request.args.get('token', None)
-    return render_template("output_short_url.html", token=token)
 
 
 @app.route('/<token>', methods=['GET', 'POST'])
