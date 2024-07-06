77fc291135e01c3e51bc3225ff0515b45cbab114
lissk0 <lissk0@mail.ru>
2024-07-06 18:33:05 +0300

    connected DataBase with main.py & flask_funcs.py

--- flask_http/flask_funcs.py
+++ flask_http/flask_funcs.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
-from db.orm_funcs import get_token_from_db, is_long_url_in_db,is_token_in_db
+from flask import Flask, render_template, request, redirect, abort
+from db.orm_funcs import DataBase
 from url_convert.url_funcs import create_short_url
 
 app = Flask(__name__)
+
+db = None
+
+
+def send_db(database):
+    global db
+    db = database
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,10 +20,10 @@ def input_long_url():
         long_url = request.form.get('longurl')
         if long_url != '':
             print(long_url)
-            if(is_long_url_in_db(long_url)):
-                token = 'http://127.0.0.1:5000/' + get_token_from_db(long_url)
+            if db.is_long_url_in_db(long_url):
+                token = 'http://127.0.0.1:5000/' + db.get_token_from_db(long_url)
             else:
-                token = 'http://127.0.0.1:5000/' + create_short_url(long_url)
+                token = 'http://127.0.0.1:5000/' + create_short_url(db, long_url)
             return render_template("output_short_url.html", token=token)
         else:
             return render_template("error_long_url_is_None.html")
@@ -24,9 +31,8 @@ def input_long_url():
 
 @app.route('/<token>', methods=['GET', 'POST'])
 def process(token):
-    if is_token_in_db(token):
-        long_url = "0" # get_longurl_from_db(token)
+    if db.is_token_in_db(token):
+        long_url = db.get_long_url_from_db(token)
         return redirect(long_url)
     else:
         return abort(404)
-
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from db.orm_funcs import create_db, connect_to_db , get_token_from_db, print_table
-from flask_http.flask_funcs import app, process, input_long_url
-# from url_convert.url_funcs import create_token, receive_all_tokens
+from db.orm_funcs import DataBase
+from flask_http.flask_funcs import app, process, input_long_url, send_db
 import sys
 from db.credentials_funcs import get_credentials, check_credentials
 
@@ -8,21 +7,11 @@ DB_NAME = "db_url"
 DATA = postgre_login, postgre_pass = get_credentials(".env")
 
 if __name__ == '__main__':
-    app.run()
-
     if not check_credentials(*DATA):
         print("!ERROR bad credentials. Closing the program.")
         sys.exit()
 
-    create_db(DB_NAME, DATA)  # creating db if not
-    engine = connect_to_db(DB_NAME, DATA)  # creating engine to manage db
-    # receive_all_tokens(engine)  # url_funcs.py needs engine to receive all tokens from db
-    print_table(engine)
+    db = DataBase(DB_NAME, DATA)
+    send_db(db)
 
-    # long_url = get_long_url()  # flask_http
-    #
-    # url_short_token = get_token_from_db(engine, long_url)  # db
-    # if url_short_token is None:
-    #     url_short_token = create_token(engine, long_url)  # url
-    #
-    # send_long_url()  # flask_http / console print
+    app.run()
