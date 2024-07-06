33475799c1babc82da2c0902456f1260b6b6e726
lissk0 <105405909+LiSSk0@users.noreply.github.com>
2024-07-06 16:05:45 +0500

    Url convert work (#3)
    * +generate_token, +create_short_url
    
    * -receive_all_tokens

--- url_convert/url_funcs.py
+++ url_convert/url_funcs.py
@@ -1,2 +1,22 @@
-def create_short_url():
-    return "https://short.url/"
+import random
+import string
+from db.orm_funcs import get_tokens, insert_to_db
+
+
+# generates unique token
+def generate_token(engine):
+    all_tokens = get_tokens(engine)
+    characters = string.ascii_letters + string.digits
+
+    token = ''.join(random.choices(characters, k=6))
+    while token in all_tokens:
+        token = ''.join(random.choices(characters, k=6))
+
+    return token
+
+
+# creates short url for long url
+def create_short_url(engine, long_url):
+    token = generate_token(engine)
+    insert_to_db(engine, long_url, token)
+    return token
