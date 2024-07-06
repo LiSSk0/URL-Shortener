4c9d43d77f3ecf89dcf7e708c9b8fc415c48a188
kurlik-da <dasha_soloveva_2005@mail.ru>
2024-07-06 13:37:27 +0300

    +.env & .env.example & -credentials.txt

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from db.orm_funcs import create_db, connect_to_db , get_token_from_db, print_table
 from flask_http.flask_funcs import get_long_url, send_long_url
-from url_convert.url_funcs import create_token, receive_all_tokens
+# from url_convert.url_funcs import create_token, receive_all_tokens
 import sys
 from db.credentials_funcs import get_credentials, check_credentials
 
 DB_NAME = "db_url"
-DATA = postgre_login, postgre_pass = get_credentials("db/credentials.txt")
+DATA = postgre_login, postgre_pass = get_credentials(".env")
 
 if __name__ == '__main__':
 
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     # receive_all_tokens(engine)  # url_funcs.py needs engine to receive all tokens from db
     print_table(engine)
 
-    long_url = get_long_url()  # flask_http
-
-    url_short_token = get_token_from_db(engine, long_url)  # db
-    if url_short_token is None:
-        url_short_token = create_token(engine, long_url)  # url
-
-    send_long_url()  # flask_http / console print
+    # long_url = get_long_url()  # flask_http
+    #
+    # url_short_token = get_token_from_db(engine, long_url)  # db
+    # if url_short_token is None:
+    #     url_short_token = create_token(engine, long_url)  # url
+    #
+    # send_long_url()  # flask_http / console print
