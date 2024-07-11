43e30c3c750c4d133c78bb2f1cc0091eb6c3d866
kurlik-da <dasha_soloveva_2005@mail.ru>
2024-07-11 18:54:53 +0300

    + QR-code

--- flask_http/flask_funcs.py
+++ flask_http/flask_funcs.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, abort
 from db.orm_funcs import DataBase
 from url_convert.url_funcs import create_short_url
+import qrcode
 
 app = Flask(__name__)
 
@@ -12,6 +13,16 @@ def send_db(database):
     db = database
 
 
+def generate_qr_code(url):
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    return qr
+
 @app.route('/', methods=['GET', 'POST'])
 def input_long_url():
     if request.method == 'GET':
@@ -21,10 +32,14 @@ def input_long_url():
         if long_url != '':
             print(long_url)
             if db.is_long_url_in_db(long_url):
-                token = 'http://127.0.0.1:5000/' + str(db.get_token_from_db(long_url)[0])
+                token = str(db.get_token_from_db(long_url)[0])
             else:
-                token = 'http://127.0.0.1:5000/' + str(create_short_url(db, long_url))
-            return render_template("output_short_url.html", token=token)
+                token = str(create_short_url(db, long_url))
+            short_url = 'http://127.0.0.1:5000/'+token
+            qr_code = generate_qr_code(short_url)
+            img = qr_code.make_image(fill_color="black", back_color="white")
+            img.save(f"flask_http/static/{token}.png")
+            return render_template("output_short_url.html", short_url=short_url, token=token)
         else:
             return render_template("error_long_url_is_None.html")
 
